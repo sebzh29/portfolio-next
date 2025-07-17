c91cfaeddcaf7e26a2af1dd3eb8e21dc6a80bfd5
Sebastien-Glippa <sebastien.glippa@hotmail.fr>
2025-07-17 19:52:41 +0200

    modif config tailwind ajout icon

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,29 +1,37 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { GithubIcon } from "./icons/GithubIcon";
 import { Section } from "./Section";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
+import { NotionIcon } from "./icons/NotionIcon";
 
 export const Header = () => {
   return (
     <header className="sticky top-0 py-4">
-      <Section className="flex-items-baseline">
-        <h1 className="text-lg font-bold text-primary-foreground">
-          Sébastien Glippa
-        </h1>
-        <div className="flex-1">
-          <ul>
-            <Link
-              className={cn(
-                buttonVariants({ variant: "outline" }),
-                "size-6 p-0"
-              )}
-              href="https://github.com/sebzh29"
-            >
-              <GithubIcon size={16} className="text-foreground" />
-            </Link>
-          </ul>
-        </div>
+      <Section className="flex items-baseline">
+        <h1 className="text-lg font-bold text-primary">Sébastien Glippa</h1>
+        <div className="flex-1" />
+        <ul className="flex items-center gap-2">
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href="https://github.com/sebzh29"
+          >
+            <GithubIcon size={16} className="text-foreground" />
+          </Link>
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href="https://www.linkedin.com/in/sebastien-glippa-dev"
+          >
+            <LinkedinIcon size={16} className="text-foreground" />
+          </Link>
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href="https://shorturl.at/cmq3D"
+          >
+            <NotionIcon size={16} className="text-foreground" />
+          </Link>
+        </ul>
       </Section>
     </header>
   );
--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -1,8 +1,9 @@
+import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
 export const Section = (props: PropsWithChildren<{ className?: string }>) => {
   return (
-    <section className={"max-w-3xl px-4 m-auto" + props.className}>
+    <section className={cn("max-w-3xl px-4 mx-auto", props.className)}>
       {props.children}
     </section>
   );
